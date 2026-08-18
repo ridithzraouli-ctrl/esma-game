3af77d17093b5de28ab886d9186377cc4a853705
ridithzraouli-ctrl <ridith.zraouli@gmail.com>
2026-08-18 07:23:00 +0200

    Implement camera object and update function

--- js/camera.js
+++ js/camera.js
@@ -1,1 +1,25 @@
+export const camera = {
+    x: 0,
+    y: 0
+};
 
+export function updateCamera(player, canvas, worldWidth) {
+
+    // Keep the player slightly left of center
+    const targetX = player.x - canvas.width * 0.4;
+
+    // Smooth movement
+    camera.x += (targetX - camera.x) * 0.08;
+
+    // Don't go past the beginning
+    if (camera.x < 0) {
+        camera.x = 0;
+    }
+
+    // Don't go past the end
+    const maxCameraX = worldWidth - canvas.width;
+
+    if (camera.x > maxCameraX) {
+        camera.x = maxCameraX;
+    }
+}
