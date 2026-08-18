cc7a975358bf334bbbda842ac1dc09f9a6eef50b
ridithzraouli-ctrl <ridith.zraouli@gmail.com>
2026-08-18 07:21:09 +0200

    Create levels.js

---
+++ js/levels.js
@@ -0,0 +1,52 @@
+export const WORLD_WIDTH = 4000;
+
+export const platforms = [
+    {
+        x: 0,
+        y: 470,
+        width: 900,
+        height: 70
+    },
+
+    {
+        x: 1050,
+        y: 400,
+        width: 250,
+        height: 25
+    },
+
+    {
+        x: 1450,
+        y: 350,
+        width: 200,
+        height: 25
+    },
+
+    {
+        x: 1800,
+        y: 420,
+        width: 300,
+        height: 25
+    },
+
+    {
+        x: 2300,
+        y: 330,
+        width: 250,
+        height: 25
+    },
+
+    {
+        x: 2700,
+        y: 400,
+        width: 300,
+        height: 25
+    },
+
+    {
+        x: 3200,
+        y: 470,
+        width: 800,
+        height: 70
+    }
+];
